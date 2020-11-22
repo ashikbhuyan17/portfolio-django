6e06d4debd87e64afcac97dc018ee873be041495
ashikbhuyan <ashikbhuyan.swe.diu@gmail.com>
2020-11-22 22:23:36 +0600

    Add works => poramming

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -10,9 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
-import django_heroku
+
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
